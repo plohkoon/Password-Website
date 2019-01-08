27062e52928143b228dcd3584c756d69eed4ec4f
Greg <greg.huber@outlook.com>
2019-01-08 15:52:14 -0800

    pushes all base pages to / and initializes express, passport and mongo connections

--- main.js
+++ main.js
@@ -5,39 +5,46 @@ const   url = require('url'),
         bodyParser = require('body-parser'),
         ejs = require('ejs'),
         methodOverride = require('method-override'),
-        passport = require('passport');
+        passport = require('passport'),
+        mongoose = require('mongoose'),
+        LocalStrategy = require('passport-local').Strategy,
+        session = require('express-session'),
+        userModel = require('./models/userModel');
 
 //initiating routes
-const   home = require('./routes/home'),
-        newaccount = require('./routes/newaccount'),
-        login = require('./routes/login');
+const   pages = require('./routes/pages'),
+        account = require('./routes/account');
 
 //initiating the server
 const   express = require('express'),
         serv = express(),
         port = 8080;
 
+mongoose.connect('mongodb://localhost:27017/users', {useNewUrlParser: true});
+//uses the userModel and its incorporated strategy
+passport.use(new LocalStrategy(userModel.authenticate()));
+
+passport.serializeUser(userModel.serializeUser());
+passport.deserializeUser(userModel.deserializeUser());
+
 //add bodyParser for POST requests
 serv.use(bodyParser.urlencoded(({extended:false})));
 serv.use(bodyParser.json());
-
+serv.use(session({secret: 'sessionsecret', resave: false, saveUninitialized: true, cookie: {secure: false}}));
+//setting method override
 serv.use(methodOverride('_method'));
-
+//setting the public
 serv.use(express.static("public"));
+//initializing passport and session credentials
+serv.use(passport.initialize());
+serv.use(passport.session());
 
 //changing the view engine to render EJS
 serv.set('view engine', 'ejs');
+//setting all the routes
+serv.use("/account", account);
 
-serv.all("/", (req, res) => {
-
-    res.redirect("/home");
-
-});
-
-serv.use("/home", home);
-
-serv.use("/newaccount", newaccount);
-
-serv.use("/login", login);
+//the main path for page renders
+serv.use("/", pages);
 
 serv.listen(port);
