02e17a85af01dcaf8be9e08037270d3e0ab4a888
Greg <greg.huber@outlook.com>
2019-01-31 20:55:24 -0800

    changed the navscript to follow logic for responsive web page

--- public/scripts/navScripts.js
+++ public/scripts/navScripts.js
@@ -1,24 +1,33 @@
 $(function() {
 
-    $("#arrow").click(function() {
+    $("#menuToggle").click(function() {
 
         if($(this).hasClass("active")) {
 
             $(this).removeClass("active");
-            $("#loginbox").removeClass("active");
+            $(".navBar").removeClass("active");
 
         } else {
 
             $(this).addClass("active");
-            $("#loginbox").addClass("active");
+            $(".navBar").addClass("active");
 
         }
 
     });
 
+
     $(window).scroll(() => {
 
         let keyHeight = $("#firstDiv").height() - $("#topBar").height();
+
+        if(isNaN(keyHeight)) {
+
+            keyHeight = 1;
+
+        }
+
+        console.log(keyHeight);
 
         if($(window).scrollTop() < keyHeight && !$("#topBar").hasClass("top")) {
 
